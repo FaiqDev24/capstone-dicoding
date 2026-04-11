dda559a4d42aab227a2a7f47d1246dd4c183e499
FaiqDev24 <faiqfadhlulaziz24@gmail.com>
2026-04-12 00:31:12 +0700

    finishing all features

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <footer className="bg-[#263B6A] text-white text-center py-4 mt-auto">
-            &copy; 2026 SILAPOR. All rights reserved.
+            &copy; {new Date().getFullYear()} SILAPOR. All rights reserved.
         </footer>
     );
 }
